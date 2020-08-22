b14ca488a00f5b69e9d81cb164a64a19f12fe82e
Darl Sele <darlsele@gmail.com>
2020-08-22 12:52:37 +0100

    Disabled Menu

--- fej.js
+++ fej.js
@@ -1,12 +1,13 @@
 function swipe(){
-    nav.classList.toggle('shift');
+    // nav.classList.toggle('shift');
     menu.classList.toggle('spin');
 }
 
 //Allocate Variables
 var menu = document.querySelector('.menu'); 
 var nav = document.querySelector('.nav-list');
+var item = document.querySelectorAll('.nav-list li a'); 
 
 
-menu.onclick = function(){swipe();};
+menu.onclick = function(){swipe();}; 
  
